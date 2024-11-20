95646e04f324da0ef54c271594d196f6c73c8a14
Srondon-Nimbeo <srondon@nimbeo.com>
2024-11-20 23:52:03 +0100

    Adding the mappings using the MapperStruct, so the code does not depends on a specific database. Implementing the ProductoRepository with all the methods and using the new Mapper structure.

--- platzi-market/src/main/java/com/curse/platzi_market/persistence/entity/Categoria.java
+++ platzi-market/src/main/java/com/curse/platzi_market/persistence/entity/Categoria.java
@@ -1,7 +1,6 @@
 package com.curse.platzi_market.persistence.entity;
 
 import jakarta.persistence.*;
-
 import java.util.List;
 
 @Entity
---
+++ platzi-market/src/main/java/com/curse/platzi_market/persistence/mapper/CategoryMapper.java
@@ -0,0 +1,23 @@
+package com.curse.platzi_market.persistence.mapper;
+
+import com.curse.platzi_market.domain.Category;
+import com.curse.platzi_market.persistence.entity.Categoria;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+
+    @Mappings({
+            @Mapping(source = "idCategoria", target = "categoryId"),
+            @Mapping(source = "descripcion", target = "category"),
+            @Mapping(source = "estado", target = "active")
+    })
+    Category toCategory(Categoria categoria);
+
+    @InheritInverseConfiguration //Notación para indicar que es básicamente la misma pero a la inversa, por eso no la definimos de nuevo.
+    @Mapping(target = "productos", ignore = true)
+    Categoria toCategoria(Category category);
+}
